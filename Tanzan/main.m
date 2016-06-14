177018227055a11aff05489821ea4b1e54598541
William Zhang <17zhangw@asij.ac.jp>
2016-06-14 15:14:41 +0900

    Initial Commit

---
+++ Tanzan/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tanzan
+//
+//  Created by williamzhang on 6/14/16.
+//  Copyright © 2016 William-Trademarks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
